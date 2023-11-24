6c6992f9f34b2345a73f2291b5d440d3259cfdcd
kavindu25 <kavindu25@github.com>
2023-11-24 18:42:04 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import CampSection from "@/components/CampSection";
+import FeaturesSection from "@/components/FeaturesSection";
+import GetAppSection from "@/components/GetAppSection";
+import GuideSection from "@/components/GuideSection";
+import HeroSection from "@/components/HeroSection";
+
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection/>
+      <CampSection/>
+      <GuideSection/>
+      <FeaturesSection/>
+      <GetAppSection/>
+    </>
+  )
+}
